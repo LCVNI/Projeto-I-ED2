12e127301822870ed4e8ad1dec04d7339901d4c4
Felipe <arthurfelipessousa@gmail.com>
2026-09-01 20:54:39 -0300

    introducao a ideai do projeto

---
+++ corpo.cpp
@@ -0,0 +1,8 @@
+#include <iostrean>
+using namespace std;
+
+struct Dados{
+  int vetor[1000];
+  int todaElementos = 1000;
+};
+
---
+++ seletion-sort.cpp
@@ -0,0 +1,44 @@
+#ifndef SELECTION-SORT_H
+#define SELECTION-SORT_H
+
+#include "corpo.cpp"
+#include <iostream>
+using namespace std;
+
+//importando a struct
+Dados dados;
+
+//carregando dados da struct
+void carregarDados(Dados novalista){
+ dados = novaLista;
+}
+
+
+//mostra os dados da strcut
+Dados obterdados(){
+ return dados;
+}
+
+
+void dados(int indice, int valor){
+ if(indice >= 0 && indice <dados.tamanho){
+ dados.vetor[indice] = valor;
+ }
+}
+
+void ordenar(){
+ for(int i = 0; i <dados.tamanho - 1; 1++){
+ int indiceMenor = i;
+
+ for(int j = i+1; j<dados.tamanho; j++){
+ if(dados.vetor[j]<dados.vetor[indiceMenor])
+  indiceMenor = j;
+   }
+ }
+ if(indiceMenor !=i){
+  int temporario = dados.vetor[i];
+  dados.vetor[i] = dados.vetor[indiceMenor;
+  dados.vetor[indicceMenor] =  temporario;
+    }
+ }
+}
