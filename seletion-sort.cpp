ea1c38916ea1406e8874fe47b7750e2398ce0a2d
Felipe <arthurfelipessousa@gmail.com>
2026-09-01 21:02:14 -0300

    Fix index initialization for selection sort
    Corrected the logic for finding the smallest index in the selection sort algorithm.

--- seletion-sort.cpp
+++ seletion-sort.cpp
@@ -19,17 +19,18 @@ Dados obterdados(){
  return dados;
 }
 
-
+//add no indice  e verifica se cabe no vetor
 void dados(int indice, int valor){
  if(indice >= 0 && indice <dados.tamanho){
  dados.vetor[indice] = valor;
  }
 }
-
+//faz a ordenacao com base no numero 
 void ordenar(){
  for(int i = 0; i <dados.tamanho - 1; 1++){
- int indiceMenor = i;
+ int indiceMenor = i; //assume que é o menor
 
+ //procura os numeros menores com base no menor numero anterior
  for(int j = i+1; j<dados.tamanho; j++){
  if(dados.vetor[j]<dados.vetor[indiceMenor])
   indiceMenor = j;
